6f19548a20b95b697a4194055e4bbb24ab5cfb6a
tulqin <tolqinmirsaliyev@gmail.com>
2023-10-31 10:39:06 +0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      
+      <div className="card text-center tush ">
+        <div className="card-header bg-primary">
+          Featured
+        </div>
+        <div className="card-body">
+         
+         
+          <a href="/" className="btn btn-primary">Go somewhere</a>
+        </div>
+        <div className="card-footer text-body-secondary">
+          2 days ago
+        </div>
+      </div>
+
+        
+    </div>
+  )
+}
+
+export default Footer
